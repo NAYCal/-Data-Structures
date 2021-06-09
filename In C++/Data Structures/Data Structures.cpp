0537efb4a4e848d31dd784357624d3612491dbb8
NAYCal <55778917+NAYCal@users.noreply.github.com>
2021-06-09 13:42:49 -0700

    Update Data Structures.cpp
    Added better  way to access circular arrays' features

--- In C++/Data Structures/Data Structures.cpp
+++ In C++/Data Structures/Data Structures.cpp
@@ -2,39 +2,71 @@
 //
 
 #include <iostream>
+#include <string>
 #include "Circular_array.h"
 
 void circular_arrays();
+bool is_digit(std::string input);
 
 int main()
 {
 	circular_arrays();
 }
 
+// checks if user input is a digit
+bool is_digit(std::string input) {
+
+	for (int i = 0; i < input.length(); i++) {
+		if (!isdigit(input[i])) {		// if this input is not a digit
+			return false;
+		}
+	}
+
+	return true;	// the only way to reach here is if everything is a digit
+
+}
 
 // function that summons circular arrays
 void circular_arrays() {
 	
-	int u_input = 0, nums = 0, sizes = 0;
+	std::string u_input;
+	int nums = 0;
 	bool valid = false;
 
-	// a do while loop to make sure we get the right input data type
+	// a do while loop to make sure we get the right data type for the size
 	do {
-		std::cout << "What number would you like to add to the Binary Tree?\n";
+		std::cout << "What size would you like for the circular array?\n";		// prompt
 
-		std::cin >> u_input;
-		if (!std::cin >> u_input) {
-			std::cin.clear();
-			std::cin.ignore(254, '\n');
-			valid = false;
+		std::cin >> u_input;		// get user input
+
+		if (!is_digit(u_input)) {		// if we do not get the right data type
+			valid = false;		// continue our loop
+			std::cout << "Invalid input! Please enter an integer.\n";
 		}
 		else {
-	
-			valid = true;
-			nums++;
+			valid = true;		// exit our loop
+		}
+		
+	} while (!valid);
+
+	C_array list(std::stoi(u_input));		// initialize our list
+
+	// a do while loop to get all the data to our list
+	do {
+		std::cout << "Enter a number for the list, and anything else to exit.\n";		// prompt
+		std::cin >> u_input;		// get input
+
+		if (is_digit(u_input)) {
+			list.push(std::stoi(u_input));		// data is pushed into the list
+
+			std::cout << "\nThe list: ";
+			list.out_order();
+			std::cout << "\n";
+		}
+		else {
+			valid = false;		// exit the loop
 		}
 
-	} while ((!valid) && (nums < sizes));
-
+	} while (valid);
 
 }
